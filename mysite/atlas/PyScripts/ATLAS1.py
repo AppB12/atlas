57e93382cd235974bac941f0444681b1d9b917f0
AppB12 <aparna.b1207@gmail.com>
2017-03-09 11:49:27 +0530

    Latest Update ATLAS1.py

--- mysite/atlas/PyScripts/ATLAS1.py
+++ mysite/atlas/PyScripts/ATLAS1.py
@@ -13,17 +13,8 @@ print("Imports complete")
 
 # Global variables
 integ_data_frame = []
-
-# ############################################################################
-
-
-# To take keywords to search for as input from user
-def keywords_input():
-    keyword_list = list()
-    kw = raw_input("Keyword:")
-    logging.info("Keyword:" + kw)
-    keyword_list.append(kw)
-    return keyword_list
+output_file_path = "C:\Users\Aparna.harihara\PycharmProjects\AuScer\Outputs\\ATLAS_Universal.csv"
+status_code = 500
 
 # ############################################################################
 
@@ -31,35 +22,19 @@ def keywords_input():
 # To remove empty rows created while integrating dataframes
 def clean_integ_dataframe(final_df):
     final_df1 = final_df[final_df.pURL != " "]
+    final_df1 = final_df1.dropna(axis='columns', how='all')
     return final_df1
 
 # ############################################################################
 
 
-# Checks if user wants to run Auscer again
-def scrape_again():
-    global run_again
-    y_n = raw_input("Do you want to scrape another site (Y/N)? ")
-    logging.info("Do you want to scrape another site (Y/N)? " + y_n)
-    if y_n == 'y' or y_n == 'Y':
-        run_again = True
-    elif y_n == 'n' or y_n == 'N':
-        run_again = False
-    else:
-        print "Bad input! Exiting..."
-        logging.info("Bad input! Exiting...")
-        run_again = False
-    return run_again
-
-##############################################################################
-
-
-
 # Main function:
-def main(kw):
+def main(kw_str):
+    global integ_data_frame
+    
     # For logging
     curr_timestamp = datetime.now().strftime("%d%B%Y_%I%M%S%p")
-    log_file_name = 'AuScerLog_' + curr_timestamp + '.log'
+    log_file_name = 'ATLASLog_' + curr_timestamp + '.log'
     full_path = dbConfig.dict["logUrl"] +log_file_name
     #logging.basicConfig(filename=full_path, level=logging.INFO)
 
@@ -71,64 +46,63 @@ def main(kw):
     print "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
     logging.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
-    # Initializations
-    run_again = True
-    what_to_scrape = None
+    integ_data_frame = pd.DataFrame({'siteCode': [' '],
+                                     'pCategory': [' '],
+                                     'pBrand': [' '],
+                                     'pTitle': [' '],
+                                     'pModel': [' '],
+                                     'price': [' '],
+                                     'pURL': [' '],
+                                     'rUser': [' '],
+                                     'rTitle': [' '],
+                                     'rDate': [' '],
+                                     'rRating': [' '],
+                                     'rText': [' '],
+                                     'rURL': [' ']}, index=[0])
 
-    # Main loop
-    while run_again:
-        integ_data_frame = pd.DataFrame({'siteCode': [' '],
-                                         'pBrand': [' '],
-                                         'pTitle': [' '],
-                                         'pModel': [' '],
-                                         'price': [' '],
-                                         'pURL': [' '],
-                                         'rUser': [' '],
-                                         'rTitle': [' '],
-                                         'rDate': [' '],
-                                         'rText': [' '],
-                                         'rURL': [' ']}, index=[0])
+    time.sleep(0.5)
 
+    print "Scraping from both sites..."
+    logging.info("Scraping from both sites...")
 
-        what_to_scrape = 2
+    print "Now scraping product information and reviews from HOMEDEPOT..."
+    logging.info("Now scraping product information and reviews from HOMEDEPOT...")
+    returned_list_HD = HomeDepot.home_depot_all_info(kw_str)
 
+    integ_data_frame = integ_data_frame.append(returned_list_HD[0])
+    status_code = returned_list_HD[1]
 
-        # Product information and reviews (elif)
-        if int(what_to_scrape) == 2:
-            opt = 3
+    print "Status Code for HomeDepot: " + str(returned_list_HD[1])
+    
+    print "Now scraping product information and reviews from AMAZON (using Import.io)..."
+    logging.info("Now scraping product information and reviews from AMAZON (using Import.io)...")
+    returned_list_AM = Amazon_I1.amazon_i_all_info(kw_str)
 
+    integ_data_frame = integ_data_frame.append(returned_list_AM[0])
+    
+    if status_code == returned_list_AM[1]:
+        status_code = returned_list_AM[1]
+    else:
+        status_code = 500
 
-        if int(opt) == 3:  # Product information and reviews from both sites
-            im_y_n = 'y'
-            if im_y_n == 'y' or im_y_n == 'Y':
+    print "Status Code for Amazon: " + str(returned_list_AM[1])
 
-                # common_keywords_list = keywords_input()
-                common_keywords_list = kw
-                time.sleep(0.5)
+    final_data_frame1 = clean_integ_dataframe(integ_data_frame)
 
-                print "Scraping from both sites..."
-                logging.info("Scraping from both sites...")
-
-                print "Now scraping product information and reviews from HOMEDEPOT..."
-                logging.info("Now scraping product information and reviews from HOMEDEPOT...")
-                integ_data_frame = integ_data_frame.append(HomeDepot.home_depot_all_info(common_keywords_list))
-
-                print "Now scraping product information and reviews from AMAZON (using Import.io)..."
-                logging.info("Now scraping product information and reviews from AMAZON (using Import.io)...")
-                integ_data_frame = integ_data_frame.append(Amazon_I1.amazon_i_all_info(common_keywords_list))
-
-                final_data_frame1 = clean_integ_dataframe(integ_data_frame)
-
-                # Saving the CSV file with product information and reviews; one CSV for each product/keyword
-                curr_timestamp = datetime.now().strftime("%d%B%Y_%I%M%S%p")
-                output_file_name = 'ATLAS_' + curr_timestamp + '.csv'
-                full_path = dbConfig.dict["outputUrl"] + output_file_name
-                final_data_frame1.to_csv(full_path, index=False, encoding='utf-8')
-                print "CSV file for this product saved at location: " + full_path
-                logging.info("CSV file for this product saved at location: " + full_path)
-
-                run_again = False
-    # 'while run_again' closes here
+    '''
+    # Saving the CSV file with product information and reviews; one CSV for each product/keyword
+    curr_timestamp = datetime.now().strftime("%d%B%Y_%I%M%S%p")
+    output_file_name = 'ATLAS_' + curr_timestamp + '.csv'
+    full_path = dbConfig.dict["outputUrl"] + output_file_name
+    final_data_frame1.to_csv(full_path, index=False, encoding='utf-8')
+    print "CSV file for this product saved at location: " + full_path
+    logging.info("CSV file for this product saved at location: " + full_path)
+    '''
+    
+    with open(output_file_path, 'a') as f:
+        final_data_frame1.to_csv(f, header=False, index=False, encoding='utf-8')
+    
+    return status_code
 
 # ####################################################################################
 
